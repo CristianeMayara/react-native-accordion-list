cef10460f6e88eb843cafddf9723a1d86ded4bf7
Cristiane Mayara <cris.mayara@gmail.com>
2017-09-27 13:10:47 -0300

    Rendering ListView row using CardSection

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { ListView } from 'rect-native';
+import { ListView } from 'react-native';
 import { connect } from 'react-redux';
 import ListItem from './ListItem';
 
@@ -13,11 +13,11 @@ class LibraryList extends Component {
   }
 
   renderRow(library) {
-    <ListItem library={library}/>;
+    return <ListItem library={library}/>;
   }
 
   render() {
-    return(
+    return (
       <ListView
         dataSource={this.dataSource}
         renderRow={this.renderRow}
